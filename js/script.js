983f4b8da9b6afaf34671698e6c7b3408033c9cb
Simi O <coolkid1984@ymail.com>
2023-08-07 16:27:22 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function saySomething(whatToSay) {
+    window.alert(whatToSay);
+  }
+  
+  function add(number1, number2) {
+    return number1 + number2;
+  }
+
+  saySomething("hi");
+
+  saySomething("The sum is " + add(3,5) + ".");
+  
+
+
+
+
